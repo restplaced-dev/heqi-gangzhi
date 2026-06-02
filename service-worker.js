3da6fab3bbf38082014171d0fcb34c3268272c5f
restplaced-dev <restplaced@gmail.com>
2026-06-02 13:35:40 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,20 @@
-const CACHE_NAME = 'heqigangzhi-prototype-cache-v1';
+const CACHE_NAME = 'heqigangzhi-prototype-cache-v2';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key === CACHE_NAME ? null : caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
